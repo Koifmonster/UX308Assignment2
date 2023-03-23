7fe9b8335963f8eb10634b81570d721fde136da0
Alex Koifman <alex.koifman@live.ca>
2023-03-23 13:15:15 -0400

    change

---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { describe, it, expect } from 'vitest';
+import { renderToString } from 'react-dom/server';
+import Index from '../pages/index.page.mdx';
+import { load } from 'cheerio';
+
+describe('index test', function () {
+  it('tests for h1 of coming soon', function () {
+    const sHtml = renderToString(React.createElement(Index));
+    const $ = load(sHtml);
+    expect($('h1').html()).toBe('Coming Soon');
+  });
+  it('test building', function () {
+    const sHtml = renderToString(React.createElement(Index));
+    const $ = load(sHtml);
+    expect(sHtml).toContain('developing');
+  });
+  it('test copyright', function () {
+    const sHtml = renderToString(React.createElement(Index));
+    const $ = load(sHtml);
+    expect(sHtml).toContain('Alex Koifman');
+  });
+
+});
